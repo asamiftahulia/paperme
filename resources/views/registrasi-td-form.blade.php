17541b1923e0447b7bf11c0b3285a30a5c9721b7
Harsya <asamimor@gmail.com>
2018-05-25 10:19:56 +0700

    default type of td unbreakable

--- resources/views/registrasi-td-form.blade.php
+++ resources/views/registrasi-td-form.blade.php
@@ -67,8 +67,9 @@
                                 <div class="form-group label-floating">
                                      <b>Type Of TD</b>
                                     <select name="type_of_td" class="form-control">
-                                      <option value="1">Breakable</op  tion>
                                       <option value="2">Unbreakable</option>
+                                      <option value="1">Breakable</op  tion>
+                                     
                                     </select>
                                 </div>
                             </div>
